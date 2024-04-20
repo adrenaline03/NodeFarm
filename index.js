6bc18e711194f0da7fb988ca19c6e98e9f24bd5f
adrenaline03 <nalin.singhal03@gmail.com>
2024-04-20 11:25:14 +0530

    Server

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const http = require("http");
+
+////////////////////////////////
+// FILES
+
+// Synchronous code.
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
+
+// Asynchronous code.
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("Error reading");
+
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written!");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read file!");
+
+////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Server running at http://127.0.0.1:8000/");
+});
