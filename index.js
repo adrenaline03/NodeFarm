91aee059c953317d39f69de5222c6a4498b0463b
adrenaline03 <nalin.singhal03@gmail.com>
2024-04-22 17:09:56 +0530

    template creation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 ////////////////////////////////
 // FILES
@@ -30,10 +31,28 @@ const http = require("http");
 
 ////////////////////////////////
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
+    const pathName = req.url;
+
+    if (pathName === "/" || pathName === "/overview") {
+        res.end("This is the overview");
+    } else if (pathName === "/product") {
+        res.end("This is the product");
+    } else if (pathName === "/api") {
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            "Content-Type": "text/html",
+        });
+        res.end("<h1>Page not found!</h1>");
+    }
 });
 
 server.listen(8000, "127.0.0.1", () => {
-  console.log("Server running at http://127.0.0.1:8000/");
+    console.log("Server running at http://127.0.0.1:8000/");
 });
